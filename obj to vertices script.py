2acdaa31024f6b35f095015a10641e204521426f
redrafael08 <204200@stmichaelcollege.nl>
2025-04-02 17:18:55 +0200

    anvil toegevoegd en lookat issues opgelost

--- obj to vertices script.py
+++ obj to vertices script.py
@@ -1,48 +1,63 @@
 import random
 
-vertices = '''
-v 1.000000 -1.000000 -1.000000
-v 1.000000 -1.000000 1.000000
-v -1.000000 -1.000000 1.000000
-v -1.000000 -1.000000 -1.000000
-v 1.000000 1.000000 -1
-v 1 1.000000 1.000001
-v -1.000000 1.000000 1.000000
-v -1.000000 1.000000 -1.000000
 
-'''
+file = open('C://Users//Rafael//Documents//blender objects//sphere.obj', 'r').read()
 
-texturecoords = '''
-vt 1.000000 0.333333
-vt 1.000000 0.666667
-vt 0.666667 0.666667
-vt 0.666667 0.333333
-vt 0.666667 0.000000
-vt 0.000000 0.333333
-vt 0.000000 0.000000
-vt 0.333333 0.000000
-vt 0.333333 1.000000
-vt 0.000000 1.000000
-vt 0.000000 0.666667
-vt 0.333333 0.333333
-vt 0.333333 0.666667
-vt 1.000000 0.000000
-'''
 
-shapes = '''
-f 2/1/1 3/2/1 4/3/1
-f 8/1/2 7/4/2 6/5/2
-f 5/6/3 6/7/3 2/8/3
-f 6/8/4 7/5/4 3/4/4
-f 3/9/5 7/10/5 8/11/5
-f 1/12/6 4/13/6 8/11/6
-f 1/4/1 2/1/1 4/3/1
-f 5/14/2 8/1/2 6/5/2
-f 1/12/3 5/6/3 2/8/3
-f 2/12/4 6/8/4 3/4/4
-f 4/13/5 3/9/5 8/11/5
-f 5/6/6 1/12/6 8/11/6
-'''
+#print(repr(file))
+
+
+
+
+n = 0
+vertices = ''
+while True:
+    try:
+        startV = file.index('v ',n)
+       # print(startV)
+        endV = file.index('\n',startV)
+       # print(endV)
+      #  print(startV+2)
+        vertices += file[startV:endV]+'\n'
+       # print(file[startV:endV])
+        n = endV
+    except:
+        break
+
+
+
+n = 0
+texturecoords = ''
+while True:
+    try:
+        startV = file.index('vt ',n)
+       # print(startV)
+        endV = file.index('\n',startV)
+       # print(endV)
+      #  print(startV+2)
+        texturecoords += file[startV:endV]+'\n'
+       # print(file[startV:endV])
+        n = endV
+    except:
+        break
+
+
+n = 0
+shapes = ''
+while True:
+    try:
+        startV = file.index('f ',n)
+       # print(startV)
+        endV = file.index('\n',startV)
+       # print(endV)
+      #  print(startV+2)
+        shapes += file[startV:endV]+'\n'
+       # print(file[startV:endV])
+        n = endV
+    except:
+        break
+
+
 
 
 
