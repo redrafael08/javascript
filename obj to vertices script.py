331bd06a80b3e286e2fda3a8909ca1e53faaaa38
redrafael08 <204200@stmichaelcollege.nl>
2025-04-10 16:53:48 +0200

    rocket werkend

--- obj to vertices script.py
+++ obj to vertices script.py
@@ -1,7 +1,7 @@
 import random
 
 
-file = open('C://Users//Rafael//Documents//blender objects//grindstone.obj', 'r').read()
+file = open('C://Users//Rafael//Documents//blender objects//rocket.obj', 'r').read()
 
 
 #print(repr(file))
@@ -112,7 +112,7 @@ for shape in shapes:
 triangles = []
 for shape in newshapes:
    # print('e',shape)
-    shapecolor = random.random()/2+0.5
+    shapecolor = 1#random.random()/2+0.5
     for i in range(len(shape)-2):
        # print(shape)
         triangles.append([[newvertices[shape[0][0]-1], newtexturecoords[shape[0][1]-1]], [newvertices[shape[i+1][0]-1], newtexturecoords[shape[i+1][1]-1]], [newvertices[shape[i+2][0]-1], newtexturecoords[shape[i+2][1]-1]], shapecolor,shapecolor,shapecolor])
