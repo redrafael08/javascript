7aa84fd50ceb63f82e8153c91bc939f2819f3c93
redrafael08 <204200@stmichaelcollege.nl>
2026-02-04 14:12:38 +0100

    leave message

--- include/input.js
+++ include/input.js
@@ -69,3 +69,15 @@ document.addEventListener('keyup', (e) => {
 });
 
 
+
+
+   window.addEventListener('beforeunload', function (e) {
+   // Cancel the event
+   e.preventDefault();
+   // Chrome requires returnValue to be set
+  // e.returnValue = '';
+   });
+
+   window.onbeforeunload=function(){return "Navigating away will lose the changes you've made to your code."};
+
+
